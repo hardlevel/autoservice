3ccb4397774d41e87c1cf8635c25334e793e80ac
Rafael Tuneca <rtuneca@grupodisal.com.br>
2025-03-17 14:37:30 -0300

    atualizar script stop para usar pm2

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 cd /var/www/autoservice
 
-APP_PORT=3000
-PID=$(lsof -t -i:$APP_PORT)
+# APP_PORT=3000
+# PID=$(lsof -t -i:$APP_PORT)
 
-if [ -n "$PID" ]; then
-  echo "Parando a aplicação na porta $APP_PORT..."
-  kill -9 $PID
-  echo "Aplicação encerrada com sucesso."
-else
-  echo "Nenhum processo rodando na porta $APP_PORT."
-fi
+# if [ -n "$PID" ]; then
+#   echo "Parando a aplicação na porta $APP_PORT..."
+#   kill -9 $PID
+#   echo "Aplicação encerrada com sucesso."
+# else
+#   echo "Nenhum processo rodando na porta $APP_PORT."
+# fi
 
-exit 0
+# exit 0
+pm2 stop all
