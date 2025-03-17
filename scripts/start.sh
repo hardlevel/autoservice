94ed11356848a50e5dfffb44ae8d7a8fedb3308e
Rafael Tuneca <rtuneca@grupodisal.com.br>
2025-03-17 14:33:38 -0300

    criar log no script do codedeploy

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 source /root/.bashrc  # Garante que o ambiente do bash seja carregado
 
+# Verifica se a pasta logs existe, se não, cria
+if [ ! -d "/var/www/autoservice/logs" ]; then
+  echo "Criando diretório logs..."
+  mkdir -p /var/www/autoservice/logs
+fi
+
+# Verifica se o arquivo de log autoservice.log existe, se não, cria
+if [ ! -f "/var/www/autoservice/logs/autoservice.log" ]; then
+  echo "Criando arquivo de log autoservice.log..."
+  touch /var/www/autoservice/logs/autoservice.log
+fi
+
+# Continuando com o restante do script
 cd /var/www/autoservice
 pnpm install
 pnpm run build
 cd /var/www/autoservice/laravel
 composer install
 cd ..
-# pnpm run start:prod || true
-pnpm run start:prod
+
+# Inicia o aplicativo (comentado o comando anterior)
+pnpm run start:prod || true
