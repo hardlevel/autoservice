0d227b9497b377af9f12f5222b9f198fd5b1c48f
Rafael Venão <rafaeltuneca@gmail.com>
2025-04-12 02:30:02 -0300

    test

--- src/autoservice/queue.service.ts
+++ src/autoservice/queue.service.ts
@@ -96,22 +96,6 @@ export class QueueService implements OnApplicationBootstrap {
         return this[queue].addBulk(data);
     }
 
-    private async handleQueue() {
-        const isPaused = await this.autoservice.isPaused();
-        if (isPaused) {
-            await this.autoservice.resume();
-            if (!(await this.autoservice.isPaused())) {
-                return true;
-            }
-        } else {
-            await this.autoservice.pause();
-            if (await this.autoservice.isPaused()) {
-                return false;
-            }
-        }
-        return isPaused;
-    }
-
     public async getQueueStatus(queue) {
         const activeJobs = await this[queue].getJobCounts();
         return activeJobs;
@@ -120,99 +104,6 @@ export class QueueService implements OnApplicationBootstrap {
     public async isQueueActive(queue) {
         const { active, waiting, delayed } = await this.getQueueStatus(queue);
         return active > 0 || waiting > 0 || delayed > 0;
-    }
-
-    public async jobAlreadyAdded(queue, data) {
-        const waitingJobs = await this[queue].getJobs(['waiting']);
-        for (const job of waitingJobs) {
-            const jobData = job.data;
-            if (jobData.startDate === data.startDate && jobData.endDate === data.endDate) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public async manageFlow(
-        year: number = 2024,
-        month: number = 1,
-        day: number = 1,
-        hour: number = 0,
-        minute: number = 0,
-        second: number = 0,
-        interval: string = '1h'
-    ) {
-        const daysInMonth = this.dates.daysInMonth(year, month);
-        const dailyJobs = [];
-
-        for (let d = day; d <= daysInMonth; d++) {
-            const hourlyJobs = [];
-
-            const startHour = d === day ? hour : 0;
-            const endHour = 24;
-
-            for (let h = startHour; h < endHour; h++) {
-                const startMinute = d === day && h === hour ? minute : 0;
-
-                for (let m = startMinute; m < 60; m += 10) {
-                    const data = { year, month, day: d, hour: h, minute: m, step: `process hour ${h} for day ${d}` };
-                    await this.addJobToQueue('hourly', data);
-                    // hourlyJobs.push({
-                    //     name: `hour-${d}-${h}-${m}`,
-                    //     queueName: 'hourly',
-                    //     data: {
-                    //         year,
-                    //         month,
-                    //         day: d,
-                    //         hour: h,
-                    //         minute: m,
-                    //         step: `process hour ${h} for day ${d}`,
-                    //     },
-                    //     opts: {
-                    //         removeOnComplete: true,
-                    //         removeOnFail: false,
-                    //     },
-                    // });
-                }
-            }
-
-            // dailyJobs.push({
-            //     name: `daily-${d}`,
-            //     queueName: 'daily',
-            //     data: {
-            //         year,
-            //         month,
-            //         day: d,
-            //         step: `process daily ${d}`,
-            //     },
-            //     opts: {
-            //         removeOnComplete: true,
-            //         removeOnFail: false,
-            //     },
-            //     children: hourlyJobs,
-            // });
-        // }
-
-        // const flow = await this.flow.add({
-        //     name: 'autoserviceFlow',
-        //     queueName: 'autoserviceFlow',
-        //     data: { year, month, day, hour, minute, second },
-        //     children: [
-        //         {
-        //             name: `monthly-${year}-${month}`,
-        //             queueName: 'monthly',
-        //             data: { year, month, step: `process monthly ${month}` },
-        //             opts: {
-        //                 removeOnComplete: true,
-        //                 removeOnFail: false,
-        //             },
-        //             children: dailyJobs,
-        //         },
-        //     ],
-        // });
-
-        // return flow;
-        }
     }
 
     @OnEvent('queue.start')
