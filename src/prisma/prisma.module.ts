9edb8f2739b10b0e2d98fe616a2237ba2f40d921
Rafael Venão <rafaeltuneca@gmail.com>
2025-04-11 23:18:57 -0300

    fix db

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { UtilModule } from '../util/util.module';
-import { PrismaSqlService } from './prisma-sql.service';
+// import { PrismaSqlService } from './prisma-sql.service';
 
 @Module({
     imports: [UtilModule],
-    providers: [PrismaService, PrismaSqlService],
-    exports: [PrismaService, PrismaSqlService]
+    providers: [PrismaService],
+    exports: [PrismaService]
 })
 export class PrismaModule { }
