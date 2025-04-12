3388670e8272326c4ef3721f3ec4cd455a6cc288
Rafael Venão <rafaeltuneca@gmail.com>
2025-04-12 19:24:07 -0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,25 @@
 module.exports = {
-  apps: [{
-    name: 'Autoservice',
-    script: 'dist/main.js',
-    // script: 'pnpm',
-    // args: 'run start:prod',
-    // cwd: '/var/www/autoservice',
-    // interpreter: "/home/ubuntu/.local/share/pnpm/node",
-    // watch: '.',
-    watch: false,
-    ignore_watch: ["[\/\\]\./", "node modules", "logs"],
-    max_size: '10M',  // Tamanho máximo do log (pode ser em MB, KB ou GB)
-    retain: '2',     // Número de arquivos antigos a manter
-    log_date_format: 'YYYY-MM-DD HH:mm Z',
-    autorestart: true,
-    env: {
-      NODE_ENV: 'development'
+  apps: [
+    {
+      name: "Autoservice",
+      script: "dist/main.js",
+      // script: 'pnpm',
+      // args: 'run start:prod',
+      // cwd: '/var/www/autoservice',
+      // interpreter: "/home/ubuntu/.local/share/pnpm/node",
+      // watch: '.',
+      watch: false,
+      ignore_watch: ["[\/\\]\./", "node modules", "logs"],
+      max_size: "10M", // Tamanho máximo do log (pode ser em MB, KB ou GB)
+      retain: "2", // Número de arquivos antigos a manter
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+      autorestart: true,
+      env: {
+        NODE_ENV: "production",
+      },
+      env_file: "/home/ubuntu/.env_prod",
     },
-    env_production: {
-      NODE_ENV: 'production',
-      GITHUB_TOKEN: process.env.GITHUB_TOKEN
-    }
-  }],
+  ],
   // deploy: {
   //   production: {
   //     user: 'ubuntu',
